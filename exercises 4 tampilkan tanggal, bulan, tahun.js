14707219fe3c561faaa813f140a9d08c322c3313
wahyudi34 <wahyudi7104@gmail.com>
2019-12-20 15:07:44 +0700

    mengganti nama file

---
+++ exercises 4 tampilkan tanggal, bulan, tahun.js
@@ -0,0 +1,7 @@
+var tanggal = 2; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 5; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun= 2020; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch(tanggal, bulan, tahun) {
+    case tanggal : { console.log(tanggal); break; }
+}
