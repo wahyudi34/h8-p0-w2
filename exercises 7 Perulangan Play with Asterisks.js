4ec67c9b08700dbe507da27ca21b49ab97f36dad
wahyudi34 <wahyudi7104@gmail.com>
2019-12-22 11:11:18 +0700

    Menghapus satu baris code

--- exercises 7 Perulangan Play with Asterisks.js
+++ exercises 7 Perulangan Play with Asterisks.js
@@ -1,23 +1,23 @@
 // 1.Menyusun barisan bintang 
 
-// var rows1 = 5;
+var rows1 = 5;
 
-// for(var i=0; i<rows1; i++) {
-//     console.log('*')
-// }   
+for(var i=0; i<rows1; i++) {
+    console.log('*')
+}   
 
 // 2. Menyusun barisan bintang dengan nested looping
 
-// var rows2 = 5;
-// var bintang = '';
+var rows2 = 5;
+var bintang = '';
 
-// for(var i=0; i<rows2; i++) {
-//     for(var j=0; j<rows2; j++) {
-//         bintang += '*'
-//     }
-//     console.log(bintang)
-//     bintang = ''
-// }
+for(var i=0; i<rows2; i++) {
+    for(var j=0; j<rows2; j++) {
+        bintang += '*'
+    }
+    console.log(bintang)
+    bintang = ''
+}
 
 // 3. Menyusun barisan tangga bintang dengan nested looping
 
@@ -25,7 +25,6 @@ var rows3 = 5;
 var bintang = ''
 
 for(var i=0; i<rows3; i++) {
-    console.log('looping ke: ' + [i])
     for(var j=i+1; j>0; j--) {
         bintang+= '*'
     }
